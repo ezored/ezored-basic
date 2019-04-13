87a943958135debe195e1eb63b98df09eb18a541
Paulo Coutinho <paulo@prsolucoes.com>
2019-04-12 22:16:44 -0300

    new config files and better macos handling

--- files/config/target_macos_app.py
+++ files/config/target_macos_app.py
@@ -1,4 +1,4 @@
-def run(params):
+def run(proj_path, target_name, params):
     return {
         'project_name': 'Sample',
         'build_types': ['Debug', 'Release'],
@@ -6,7 +6,9 @@ def run(params):
             {
                 'arch': 'x86_64',
                 'conan_arch': 'x86_64',
-                'conan_profile': 'ezored_windows_app_profile',
+                'conan_profile': 'ezored_macos_app_profile',
+                'min_version': '10.9',
             },
         ],
+        'assets_dir': 'projects/samples/basic/assets',
     }
